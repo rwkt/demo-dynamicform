eebd2f044e48e49e8a810394921cfa22cd2a59b5
africorn <37826694+africorn@users.noreply.github.com>
2020-10-25 13:26:42 +0000

    cleanup

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,21 +4,14 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('title')
-        ;
+        $builder->add('title');
     }
 
     public function configureOptions(OptionsResolver $resolver)
